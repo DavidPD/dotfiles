5e5ab1caf4b9f386f459964555558b502b94234a
David Dovel <davey5555@gmail.com>
2014-09-22 07:34:04 -0500

    Replaced zshrc with current, needs fixing

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,1 +1,10 @@
 alias reload!='. ~/.zshrc'
+
+# setup gnu utils, if available
+if [[ -d /usr/local/opt/coreutils/libexec/gnubin ]] ; then
+	export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+	export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+	unalias ls
+	alias ls="ls --color"
+	eval `dircolors ~/.dir_colors`
+fi
--- zsh/config.zsh
+++ zsh/config.zsh
@@ -39,10 +39,36 @@ setopt complete_aliases
 
 zle -N newtab
 
-bindkey '^[^[[D' backward-word
-bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char
+
+# Keypad
+# 0 . Enter
+bindkey -s "^[Op" "0"
+bindkey -s "^[Ol" "."
+bindkey -s "^[OM" "^M"
+# 1 2 3
+bindkey -s "^[Oq" "1"
+bindkey -s "^[Or" "2"
+bindkey -s "^[Os" "3"
+# 4 5 6
+bindkey -s "^[Ot" "4"
+bindkey -s "^[Ou" "5"
+bindkey -s "^[Ov" "6"
+# 7 8 9
+bindkey -s "^[Ow" "7"
+bindkey -s "^[Ox" "8"
+bindkey -s "^[Oy" "9"
+# + -  * /
+bindkey -s "^[Ok" "+"
+bindkey -s "^[Om" "-"
+bindkey -s "^[Oj" "*"
+bindkey -s "^[Oo" "/"
+
+# alt(option) arrow keys to move by words
+bindkey "^[^[[D" backward-word
+bindkey "^[^[[C" forward-word
+
