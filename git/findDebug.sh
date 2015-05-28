a55cb2487a09885d3f3ca69a923d5fd2acb59ace
David Dovel <davey5555@gmail.com>
2015-05-28 08:26:37 -0500

    Fix error in findDebug script and also made it case-insensitive

--- git/findDebug.sh
+++ git/findDebug.sh
@@ -8,6 +8,5 @@ pipe_if_not_empty () {
 FILES_PATTERN='\.(js|coffee|php)(\..+)?$'
 FORBIDDEN='DD:\s?debug'
 git diff --cached --name-only | pipe_if_not_empty \
-	GREP_COLOR='4;5;37;41' \
-	xargs grep --with-filename -n -E $FORBIDDEN && echo 'debugging references found, please remove before commiting' && exit 1
+	xargs grep --with-filename -i -n -E $FORBIDDEN && echo 'debugging references found, please remove before commiting' && exit 1
 exit 0
