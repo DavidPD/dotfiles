4ceb1690f51cc2f705a2b8a2f1fbd2c47b41162c
David Dovel <davey5555@gmail.com>
2019-05-22 10:38:45 -0500

    changes to allow git completion to work

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -28,7 +28,7 @@ setopt HIST_REDUCE_BLANKS
 
 # don't expand aliases _before_ completion has finished
 #   like: git comm-[tab]
-setopt complete_aliases
+unsetopt complete_aliases
 
 zle -N newtab
 
