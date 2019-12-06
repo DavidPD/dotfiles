89718017d092e6b7b5f0036bb78518bbe5ecf524
Davd Dovel <davey5555@gmail.com>
2019-12-06 00:11:35 -0600

    Add global git config

--- installation/install.sh
+++ installation/install.sh
@@ -1,4 +1,5 @@
 #! /usr/bin/env zsh
 
 # TODO: auto copy .example files, without overriding
-cp ~/.dotfiles/example\ configs/.zshrc.example ~/.zshrc
+cp "$HOME/.dotfiles/example configs/zshrc.example" $HOME/.zshrc
+cp "$HOME/.dotfiles/example configs/gitconfig.example" $HOME/.gitconfig
