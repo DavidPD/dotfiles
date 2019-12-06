7cce48175b5b46c5002043da68ae815c915f13aa
Davd Dovel <davey5555@gmail.com>
2019-12-05 22:20:11 -0600

    Enable oh-my-zsh and add heavily customized remy theme

--- zsh/main.zsh
+++ zsh/main.zsh
@@ -2,5 +2,9 @@ export ZSH=$HOME/.oh-my-zsh
 export DOTFILES=$HOME/.dotfiles
 export ZSH_CUSTOM=$DOTFILES/zsh/oh-my-zsh
 
+export ZSH_THEME='remy'
+
+source $ZSH/oh-my-zsh.sh
+
 # TODO: Dynamically source zsh files?
 source $DOTFILES/zsh/aliases.zsh
